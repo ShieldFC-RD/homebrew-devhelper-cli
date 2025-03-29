ad35b724758e364a2be03ece6c0617e24c661adc
GitHub Action <action@github.com>
2025-03-29 13:41:47 +0000

    Update formula to v0.1.8

--- Formula/devhelper-cli.rb
+++ Formula/devhelper-cli.rb
@@ -1,13 +1,17 @@
 class DevhelperCli < Formula
   desc "A comprehensive command-line interface for ShieldDev operations"
   homepage "https://github.com/lirtsman/devhelper-cli"
-  url "https://github.com/lirtsman/devhelper-cli/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "cbf371fa85389f5fe94e4cd8d333f10c8e5f13e55c36e3277de7fdaf18a1ac54"
+  url "https://github.com/lirtsman/devhelper-cli/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "bcd85d47769f45ca12b54fb440a9ad75a63cd9a09bb4387ae75da6414f81d34d"
   
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", "devhelper-cli"
+    # Get version information and truncate tag prefix "v"
+    version_no_v = version.to_s.sub(/^v/, "")
+    system "go", "build", 
+           "-ldflags", "-X github.com/lirtsman/devhelper-cli/cmd.Version=#{version_no_v} -X github.com/lirtsman/devhelper-cli/cmd.BuildDate=#{Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")}",
+           "-o", "devhelper-cli"
     bin.install "devhelper-cli"
   end
 
