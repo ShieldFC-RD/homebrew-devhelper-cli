9363bd32111d17b94aaaa257179e6fe75d394339
GitHub Action <action@github.com>
2025-03-29 13:46:42 +0000

    Update formula to v0.1.9

--- Formula/devhelper-cli.rb
+++ Formula/devhelper-cli.rb
@@ -1,8 +1,8 @@
 class DevhelperCli < Formula
   desc "A comprehensive command-line interface for ShieldDev operations"
   homepage "https://github.com/lirtsman/devhelper-cli"
-  url "https://github.com/lirtsman/devhelper-cli/archive/refs/tags/v0.1.8.tar.gz"
-  sha256 "bcd85d47769f45ca12b54fb440a9ad75a63cd9a09bb4387ae75da6414f81d34d"
+  url "https://github.com/lirtsman/devhelper-cli/archive/refs/tags/v0.1.9.tar.gz"
+  sha256 "ad659de1ee0cfca0a38b5de77331b21551123cd8f76c0ca65e8ee495c11b5cc8"
   
   depends_on "go" => :build
 
@@ -10,7 +10,7 @@ class DevhelperCli < Formula
     # Get version information and truncate tag prefix "v"
     version_no_v = version.to_s.sub(/^v/, "")
     system "go", "build", 
-           "-ldflags", "-X github.com/lirtsman/devhelper-cli/cmd.Version=#{version_no_v} -X github.com/lirtsman/devhelper-cli/cmd.BuildDate=#{Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")}",
+           "-ldflags", "-X github.com/lirtsman/devhelper-cli/cmd.Version=#{version_no_v} -X github.com/lirtsman/devhelper-cli/cmd.BuildDate=#{Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")} -X github.com/lirtsman/devhelper-cli/cmd.Commit=bb2fe850cf945559b300def7934663aede258cbb",
            "-o", "devhelper-cli"
     bin.install "devhelper-cli"
   end
