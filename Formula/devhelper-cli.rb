00a948b2ebaa4a74e1d0be5c30a7455dca7dc526
lirtsman <ilya.lirtsman@gmail.com>
2025-04-28 11:15:19 +0100

    Update formula to use gh CLI for downloading assets

--- Formula/devhelper-cli.rb
+++ Formula/devhelper-cli.rb
@@ -3,6 +3,8 @@ class DevhelperCli < Formula
   homepage "https://github.com/ShieldFC-RD/devhelper-cli"
   version "0.4.0"
 
+  depends_on "gh"
+
   # You need to create a GitHub personal access token and add it to your environment:
   # export HOMEBREW_GITHUB_API_TOKEN=your_token_here
   if ENV["HOMEBREW_GITHUB_API_TOKEN"].nil?
@@ -11,26 +13,35 @@ class DevhelperCli < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://#{ENV["HOMEBREW_GITHUB_API_TOKEN"]}@github.com/ShieldFC-RD/devhelper-cli/releases/download/v#{version}/devhelper-cli-darwin-arm64"
       sha256 "76706018bd6a65e1bff43d06966573be1b48cf83bbd2f979b711046b960e9a4a"
     else
-      url "https://#{ENV["HOMEBREW_GITHUB_API_TOKEN"]}@github.com/ShieldFC-RD/devhelper-cli/releases/download/v#{version}/devhelper-cli-darwin-amd64"
       sha256 "5fd1cb2fbd1cf2315d0cb09ab2f5176fb090762cc705987b1627ee8177d5cfc1"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://#{ENV["HOMEBREW_GITHUB_API_TOKEN"]}@github.com/ShieldFC-RD/devhelper-cli/releases/download/v#{version}/devhelper-cli-linux-arm64"
       sha256 "bded17592001da17b0ad424f78ef0478c9f394b617bc25afe8ac61301d563bb9"
     else
-      url "https://#{ENV["HOMEBREW_GITHUB_API_TOKEN"]}@github.com/ShieldFC-RD/devhelper-cli/releases/download/v#{version}/devhelper-cli-linux-amd64"
       sha256 "f835e01be52474f16186c156563345b383d816e890ead1ed43ea30e51f622329"
     end
   end
 
+  def binary_name
+    if OS.mac?
+      Hardware::CPU.arm? ? "devhelper-cli-darwin-arm64" : "devhelper-cli-darwin-amd64"
+    elsif OS.linux?
+      Hardware::CPU.arm? ? "devhelper-cli-linux-arm64" : "devhelper-cli-linux-amd64"
+    end
+  end
+
   def install
-    # The downloaded file is already a binary, just rename and make it executable
-    chmod 0755, Dir["*"].first
-    bin.install Dir["*"].first => "devhelper-cli"
+    # Download the binary using gh CLI
+    system "gh", "release", "download", "v#{version}", 
+           "--repo", "ShieldFC-RD/devhelper-cli",
+           "--pattern", binary_name
+    
+    # Make it executable and install
+    chmod 0755, binary_name
+    bin.install binary_name => "devhelper-cli"
   end
 
   test do
