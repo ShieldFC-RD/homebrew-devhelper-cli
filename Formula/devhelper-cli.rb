b51cbde5b55732135cf72676ed1ff43fb2e959d5
lirtsman <ilya.lirtsman@gmail.com>
2025-04-28 11:00:27 +0100

    Refactor devhelper-cli formula to support architecture-specific URLs and streamline installation process

--- Formula/devhelper-cli.rb
+++ Formula/devhelper-cli.rb
@@ -1,18 +1,28 @@
 class DevhelperCli < Formula
   desc "A comprehensive command-line interface for Shield operations"
   homepage "https://github.com/ShieldFC-RD/devhelper-cli"
-  url "https://github.com/ShieldFC-RD/devhelper-cli/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  
-  depends_on "go" => :build
+  version "0.4.0"
+
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/ShieldFC-RD/devhelper-cli/releases/download/v#{version}/devhelper-cli-darwin-arm64"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    else
+      url "https://github.com/ShieldFC-RD/devhelper-cli/releases/download/v#{version}/devhelper-cli-darwin-amd64"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/ShieldFC-RD/devhelper-cli/releases/download/v#{version}/devhelper-cli-linux-arm64"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    else
+      url "https://github.com/ShieldFC-RD/devhelper-cli/releases/download/v#{version}/devhelper-cli-linux-amd64"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    end
+  end
 
   def install
-    # Get version information and truncate tag prefix "v"
-    version_no_v = version.to_s.sub(/^v/, "")
-    system "go", "build", 
-           "-ldflags", "-X github.com/ShieldFC-RD/devhelper-cli/cmd.Version=#{version_no_v} -X github.com/ShieldFC-RD/devhelper-cli/cmd.BuildDate=#{Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")} -X github.com/ShieldFC-RD/devhelper-cli/cmd.Commit=702aef6c82c651e36441bbb968722d39f922c278",
-           "-o", "devhelper-cli"
-    bin.install "devhelper-cli"
+    bin.install Dir["*"].first => "devhelper-cli"
   end
 
   test do
