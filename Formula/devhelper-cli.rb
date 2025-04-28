d64bc01a254e5d0b0b22b60b0aaa462ffc08cb2b
lirtsman <ilya.lirtsman@gmail.com>
2025-04-28 11:18:29 +0100

    Simplify formula to use architecture-specific URLs

--- Formula/devhelper-cli.rb
+++ Formula/devhelper-cli.rb
@@ -2,46 +2,40 @@ class DevhelperCli < Formula
   desc "A comprehensive command-line interface for Shield operations"
   homepage "https://github.com/ShieldFC-RD/devhelper-cli"
   version "0.4.0"
-  url "https://github.com/ShieldFC-RD/devhelper-cli/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "76706018bd6a65e1bff43d06966573be1b48cf83bbd2f979b711046b960e9a4a"
 
   depends_on "gh"
 
-  bottle do
-    root_url "https://github.com/ShieldFC-RD/devhelper-cli/releases/download/v0.4.0"
-    
-    if OS.mac?
-      if Hardware::CPU.arm?
-        sha256 cellar: :any_skip_relocation, arm64_sonoma: "76706018bd6a65e1bff43d06966573be1b48cf83bbd2f979b711046b960e9a4a"
-      else
-        sha256 cellar: :any_skip_relocation, sonoma: "5fd1cb2fbd1cf2315d0cb09ab2f5176fb090762cc705987b1627ee8177d5cfc1"
-      end
-    elsif OS.linux?
-      if Hardware::CPU.arm?
-        sha256 cellar: :any_skip_relocation, arm64_linux: "bded17592001da17b0ad424f78ef0478c9f394b617bc25afe8ac61301d563bb9"
-      else
-        sha256 cellar: :any_skip_relocation, x86_64_linux: "f835e01be52474f16186c156563345b383d816e890ead1ed43ea30e51f622329"
-      end
+  on_macos do
+    on_arm do
+      url "https://github.com/ShieldFC-RD/devhelper-cli/releases/download/v0.4.0/devhelper-cli-darwin-arm64"
+      sha256 "76706018bd6a65e1bff43d06966573be1b48cf83bbd2f979b711046b960e9a4a"
+    end
+    on_intel do
+      url "https://github.com/ShieldFC-RD/devhelper-cli/releases/download/v0.4.0/devhelper-cli-darwin-amd64"
+      sha256 "5fd1cb2fbd1cf2315d0cb09ab2f5176fb090762cc705987b1627ee8177d5cfc1"
     end
   end
 
-  def binary_name
-    if OS.mac?
-      Hardware::CPU.arm? ? "devhelper-cli-darwin-arm64" : "devhelper-cli-darwin-amd64"
-    elsif OS.linux?
-      Hardware::CPU.arm? ? "devhelper-cli-linux-arm64" : "devhelper-cli-linux-amd64"
+  on_linux do
+    on_arm do
+      url "https://github.com/ShieldFC-RD/devhelper-cli/releases/download/v0.4.0/devhelper-cli-linux-arm64"
+      sha256 "bded17592001da17b0ad424f78ef0478c9f394b617bc25afe8ac61301d563bb9"
+    end
+    on_intel do
+      url "https://github.com/ShieldFC-RD/devhelper-cli/releases/download/v0.4.0/devhelper-cli-linux-amd64"
+      sha256 "f835e01be52474f16186c156563345b383d816e890ead1ed43ea30e51f622329"
     end
   end
 
   def install
-    # Download the binary using gh CLI
+    # Download the binary using gh CLI since the repository is private
     system "gh", "release", "download", "v#{version}", 
            "--repo", "ShieldFC-RD/devhelper-cli",
-           "--pattern", binary_name
+           "--pattern", File.basename(url)
     
     # Make it executable and install
-    chmod 0755, binary_name
-    bin.install binary_name => "devhelper-cli"
+    chmod 0755, File.basename(url)
+    bin.install File.basename(url) => "devhelper-cli"
   end
 
   test do
